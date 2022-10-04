9252c7b2b9d6c584fa30b5b1c32f64572641cbe8
Bartol Deak <b@bdeak.net>
2022-10-04 22:59:47 +0200

    image versioning and cleanup

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Build NixOS image for Azure Virtual Machine
 
 Forked from <https://github.com/NixOS/nixpkgs/tree/master/nixos/maintainers/scripts/azure-new>
 
+Script currently builds NixOS version 22.05. When new NixOS version arrives, change version in `upload-image.sh` and `image.nix` and run script again.
+
 ## Usage
 
 ```sh
--- system.nix
+++ system.nix
@@ -1,29 +1,10 @@
 { pkgs, modulesPath, ... }:
-let
-  username = "dumpovac";
-  authorizedKeysURL = "";
-  authorizedKeysSHA = "";
-in
 {
   imports = [
     "${modulesPath}/virtualisation/azure-common.nix"
     "${modulesPath}/virtualisation/azure-image.nix"
   ];
 
-  users.users."${username}" = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    # make this part more dynamic
-    #openssh.authorizedKeys.keys = let
-    #  authorizedKeys = pkgs.fetchurl {
-    #    url = "<url to public keys>";
-    #    sha256 = "1kril7clfay225xdfhpp770gk60g5rp66nr6hzd5gpxvkynyxlrf";
-    #  };
-    #in pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuAQ43SM0EVulTuivIuAGI0P2RcREUY0nTRtlolZDZ bartol@dump.hr"
-    ];
-  };
   services.openssh.passwordAuthentication = false;
   security.sudo.wheelNeedsPassword = false;
 
@@ -31,7 +12,7 @@ in
     vim git tmux htop curl wget
   ];
 
-  virtualisation.azureImage.diskSize = 2500;
+  virtualisation.azureImage.diskSize = 32000;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
   system.stateVersion = "22.05";
